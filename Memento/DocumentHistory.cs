664f51ac2fa745477713f3fa3039c1d5c22ce0ee
oybekabdullaev <o.abdullaev2@student.inha.uz>
2020-12-04 21:28:07 +0500

    Add memento pattern.

---
+++ Memento/DocumentHistory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Memento
+{
+    class DocumentHistory
+    {
+        private readonly Stack<DocumentState> _states;
+
+        public DocumentHistory()
+        {
+            _states = new Stack<DocumentState>();
+        }
+
+        public void Push(DocumentState state)
+        {
+            _states.Push(state);
+        }
+
+        public DocumentState Pop()
+        {
+            return _states.Pop();
+        }
+    }
+}
---
+++ Memento/Main.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.Memento
+{
+    static class Main
+    {
+        public static void Test()
+        {
+            var document = new Document();
+            var history = new DocumentHistory();
+
+            document.Content = "Hello";
+
+            history.Push(document.CreateState());
+            document.FontName = "Font 1";
+
+            history.Push(document.CreateState());
+            document.FontSize = 14;
+
+            Console.WriteLine(document);
+
+            document.Restore(history.Pop());
+            Console.WriteLine(document);
+
+            document.Restore(history.Pop());
+            Console.WriteLine(document);
+        }
+    }
+}
